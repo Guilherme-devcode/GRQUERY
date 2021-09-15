1e02d87114b4a823e6abf64a65b425e62209ec6b
Guilherme mesquita rocha <guilherme.mesquita.rocha@gmail.com>
2021-09-14 22:06:16 -0300

    primeiro commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,47 +4,48 @@
 
 
 <!-- Banner Image  -->
-<div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
-  <div class="content text-center">
-    <h1 class="text-white">GR QUERY</h1>
-  </div>
+<div id="search-container" class="col-md-12">
+  <h1 class="text-white">GR QUERY</h1>
+    <div class="search-box" align="center">
+      <input class="search-txt" type="text" name="" placeholder="Faça a sua pesquisa">
+      <a class="search-btn" href="#">
+          <i class="fas fa-search"></i>
+      </a>
+    </div>
 </div>
+    
+
+
+
+<br>
 
 <!-- Main Content Area -->
-<div class="container my-5 d-grid gap-5">
-  <div class="p-5 border">
-    <p>
-     TEXTO BASE.
-    </p>
-  </div>
-  <div class="p-5 border">
-    <p>
-      TEXTO BASE.
-    </p>
-  </div>
-  <div class="p-5 border">
-    <p>
-      TEXTO BASE.
-    </p>
-  </div>
-  <div class="p-5 border">
-    <p>
-     TEXTO BASE.
-    </p>
+<div id="about-area">
+  <div class="container">
+      <div class="row">
+          <div class="col-12">
+              <h3 class="main-about-title">Sobre a GR QUERY</h3>
+          </div>
+          <div class="col-md-6">
+              <img class="img-fluid" src="img/banner-2.jpg" alt="Agência hDC">
+          </div>
+          <div class="col-md-6">
+              <h3 class="about-title">Uma agência que pensa no futuro</h3>
+              <p>Nossos projetos são adaptados ao cliente e seu propósito.</p>
+              <p>empresa especializada em administração de consultas médicas, trazendo consigo uma ampla fonte de opções para administrar e registrar consultas e agendamentos, atráves de um sistema utilizando banco de dados e desenvolvimento web, garantido uma forma segura e eficáz de fazer agendamentos e cadastro de pascientes. Com suporte da nossa equipe, quaisquer duvidas ou questionamentos será resolvida com uma rápida equipe com missão de trazer satisfação para nossos clientes.</p>
+              <p>Funções do sistema</p>
+              <ul id="about-list">
+                  <li><ion-icon name="checkmark-outline"></ion-icon>Utilização de Machine Learning</li>
+                  <li><ion-icon name="checkmark-outline"></ion-icon></i>Layout responsivo para todos os dispositivos</li>
+                  <li><ion-icon name="checkmark-outline"></ion-icon>Integração com diversos sistemas do mercado</li>
+                  <li><ion-icon name="checkmark-outline"></ion-icon>Sistema de pagamento próprio</li>
+                  <li><ion-icon name="checkmark-outline"></ion-icon>Desenvolvimento com metodologia ágil</li>
+              </ul>
+          </div>
+      </div>
   </div>
 </div>
 
-<!--navbar transparente js -->
-<script src="js/bootstrap.bundle.min.js"></script>
-<script type="text/javascript">
-  var nav = document.querySelector('nav');
-  window.addEventListener('scroll', function () {
-    if (window.pageYOffset > 100) {
-      nav.classList.add('bg-dark', 'shadow');
-    } else {
-      nav.classList.remove('bg-dark', 'shadow');
-    }
-  });
-</script>
+
 
 @endsection 
--- resources/views/layouts/sidebarmenu.blade.php
+++ resources/views/layouts/sidebarmenu.blade.php
@@ -13,6 +13,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
 
 </head>
 
@@ -99,7 +103,18 @@
 
 
 
-
+<!--navbar transparente js -->
+<script src="js/bootstrap.bundle.min.js"></script>
+<script type="text/javascript">
+  var nav = document.querySelector('nav');
+  window.addEventListener('scroll', function () {
+    if (window.pageYOffset > 100) {
+      nav.classList.add('bg-dark', 'shadow');
+    } else {
+      nav.classList.remove('bg-dark', 'shadow');
+    }
+  });
+</script>
 
 </body>
 </html>
