0a0d1bf64735f98366114b789fcff7903017a1de
Guilherme mesquita rocha <guilherme.mesquita.rocha@gmail.com>
2021-09-15 21:50:46 -0300

    Segundo commit

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\QueryController;
 
-
+Route::get('/home', [QueryController::class, 'index']);
 
 Route::get('/', function () {
     return view('layouts.sidebarmenu');
 });
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
